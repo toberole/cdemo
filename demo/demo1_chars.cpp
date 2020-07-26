180e0ce0cba39203ecc38ccf21601cccd4f77f55
zhouwei441300 <zhouwei441300@163.com>
2020-07-26 20:38:54 +0800

    note

--- demo/demo1_chars.cpp
+++ demo/demo1_chars.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#include <memory>
 #include <string>
 
 // 判断中文
@@ -10,143 +10,82 @@
 
 using namespace std;
 
-wchar_t* MBCS2Unicode(wchar_t * buff, const char * str){
+wchar_t *MBCS2Unicode(wchar_t *buff, const char *str) {
 
-    wchar_t * wp = buff;
+    wchar_t *wp = buff;
 
-    char * p = (char *)str;
+    char *p = (char *) str;
 
-    while (*p)
-
-    {
-
-        if (*p & 0x80)
-
-        {
-
-            *wp = *(wchar_t *)p;
-
+    while (*p) {
+        if (*p & 0x80) {
+            *wp = *(wchar_t *) p;
             p++;
-
+        } else {
+            *wp = (wchar_t) *p;
         }
-
-        else {
-
-            *wp = (wchar_t)*p;
-
-        }
-
         wp++;
-
         p++;
-
     }
-
     *wp = 0x0000;
-
     return buff;
 }
 
-char * Unicode2MBCS(char * buff, const wchar_t * str)
-
-{
-
-    wchar_t * wp = (wchar_t *)str;
-
-    char * p = buff, *tmp;
-
+char *Unicode2MBCS(char *buff, const wchar_t *str) {
+    wchar_t *wp = (wchar_t *) str;
+    char *p = buff, *tmp;
     while (*wp) {
-
-        tmp = (char *)wp;
-
+        tmp = (char *) wp;
         if (*wp & 0xFF00) {
-
             *p = *tmp;
-
-            p++; tmp++;
-
-            *p = *tmp;
-
             p++;
-
-        }
-
-        else {
-
+            tmp++;
             *p = *tmp;
-
             p++;
-
+        } else {
+            *p = *tmp;
+            p++;
         }
-
         wp++;
-
     }
-
     *p = 0x00;
-
     return buff;
 }
 
-wstring str2wstr(string str){
-
+wstring str2wstr(string str) {
     size_t len = str.size();
-
-    wchar_t * b = (wchar_t *)malloc((len + 1) * sizeof(wchar_t));
-
+    wchar_t *b = (wchar_t *) malloc((len + 1) * sizeof(wchar_t));
     MBCS2Unicode(b, str.c_str());
-
     wstring r(b);
-
     free(b);
-
     return r;
-
 }
 
-int wputs(const wchar_t * wstr);
+int wputs(const wchar_t *wstr);
 
-int wputs(wstring wstr){
-
+int wputs(wstring wstr) {
     wputs(wstr.c_str());
-
     return 0;
 }
 
-
-int wputs(const wchar_t * wstr){
-
+int wputs(const wchar_t *wstr) {
     int len = wcslen(wstr);
-
-    char * buff = (char *)malloc((len * 2 + 1) * sizeof(char));
-
+    char *buff = (char *) malloc((len * 2 + 1) * sizeof(char));
     Unicode2MBCS(buff, wstr);
-
     printf("%s", buff);
-
     free(buff);
-
     return 0;
 }
 
-string wstr2str(wstring wstr){
-
+string wstr2str(wstring wstr) {
     size_t len = wstr.size();
-
-    char * b = (char *)malloc((2 * len + 1) * sizeof(char));
-
+    char *b = (char *) malloc((2 * len + 1) * sizeof(char));
     Unicode2MBCS(b, wstr.c_str());
-
     string r(b);
-
     free(b);
-
     return r;
 }
 
-int main(){
-    
-
+int main() {
     //输入层：接收char*输入，并将其转换为wchar*
     string input = "今天你在武汉！！";
 
@@ -159,18 +98,13 @@ int main(){
 
     wstring temp_buff = str2wstr(temp);
 
-    cout << "input的中文个数:"<<wcslen(buff.c_str()) << "   temp的中文个数:" << wcslen(temp_buff.c_str()) << endl;
+    cout << "input的中文个数:" << wcslen(buff.c_str()) << "   temp的中文个数:" << wcslen(temp_buff.c_str()) << endl;
 
     cout << "判断input第三个是否是'在':" << (buff[3] == temp_buff[0]) << endl;;
-
-
 
     //输出层将wchar*的结果再次转换为char*，返回给外部
 
     wputs(buff);
 
-
-
     return 0;
-
 }
---
+++ demo/for_auto.cpp
@@ -0,0 +1,31 @@
+﻿#include <vector>
+#include <iostream>
+#include <memory>
+/*
+for(auto x : range)创建拷贝[注意效率]，无法修改range中的元素
+
+for(auto& x : range)可以修改range中的元素，但一般用以下这种
+for(auto&& x : range)
+
+for(const auto & x : range)只读range中的元素
+*/
+int main(int argc, char const *argv[])
+{
+
+    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (auto iter = v.cbegin(); iter != v.cend(); iter++)
+    {
+        std::cout << (*iter) << std::endl;
+    }
+
+    std::cout << "-------" << std::endl;
+    for (auto &&i : v)
+    {
+        std::cout << i << std::endl;
+    }
+
+    int *ip = new int;
+    std::shared_ptr<int> sp(ip);
+    std::cout << (*ip) << std::endl;
+    return 0;
+}
