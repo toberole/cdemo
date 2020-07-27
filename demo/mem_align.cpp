d337f40e43de1ac825f808f3db8f2d218b3d51c4
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-07-27 17:07:11 +0800

    内存对齐

---
+++ demo/mem_align.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// 注意内存对齐系数一般是平台的对应的位数，
+// 比如64位 对齐系数默认是8，32位默认对齐系数是4
+//#pragma pack(16) // 设定对齐系数为16
+
+struct Test {
+    char x1;
+    double x2;
+    short x3;
+    float x4;
+    char x5;
+};
+
+int main() {
+    std::cout << "double size: " << sizeof(double) << std::endl;
+    std::cout << "short size: " << sizeof(short) << std::endl;
+    std::cout << "float size: " << sizeof(float) << std::endl;
+    std::cout << "Test size: " << sizeof(Test) << std::endl;
+    return 0;
+}
