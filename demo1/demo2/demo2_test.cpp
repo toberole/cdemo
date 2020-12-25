bb71d9af5b41312bb0ef5640fdf2a32345482978
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-12-25 23:05:46 +0800

    note

---
+++ demo1/demo2/demo2_test.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include<iostream>
+int main(int argc, char const *argv[])
+{
+    std::string s = "";
+    std::cout << s.size() << std::endl;
+
+    const char*strpath = "hello";
+
+    return 0;
+}
