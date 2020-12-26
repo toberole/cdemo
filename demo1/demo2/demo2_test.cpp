e4d84af114633d509d922f315a2f59ee3c64a907
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-12-26 21:15:20 +0800

    note

--- demo1/demo2/demo2_test.cpp
+++ demo1/demo2/demo2_test.cpp
@@ -1,11 +1,30 @@
 #include <string>
-#include<iostream>
+#include <iostream>
+
+void arg_cnt(int cnt, ...)
+{
+    int value=0;
+    int i=0;
+    int arg_cnt=cnt;
+    va_list arg_ptr; 
+    va_start(arg_ptr, cnt); 
+    for(i = 0; i < cnt; i++)
+    {
+        value = va_arg(arg_ptr,int);
+        printf("value%d=%d\n", i+1, value);
+    }
+    va_end(arg_ptr);
+
+}
+
 int main(int argc, char const *argv[])
 {
     std::string s = "";
     std::cout << s.size() << std::endl;
 
-    const char*strpath = "hello";
+    const char *strpath = "hello";
+
+    arg_cnt(3,1,2,3);
 
     return 0;
 }
