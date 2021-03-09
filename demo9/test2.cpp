12c27760a6b2522f0856fe8bd73d6698863dca41
zhouwei215664 <zhouwei215664@sogou-inc.com>
2021-03-09 20:33:20 +0800

    note

--- demo9/test2.cpp
+++ demo9/test2.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
     const char *buf;
     std::string s = std::string(buf, 0);
     s = std::string(buf, 0);
-    std::cout << "s: " << s <<",size: "<<s.size()<< std::endl;
+    std::cout << "s: " << s << ",size: " << s.size() << std::endl;
     std::string s1 = std::move(s);
     std::cout << "s1: " << s1 << std::endl;
-    
+
+    char*p = nullptr;
+    printf("p: %p",p);
+
     return 0;
 }
